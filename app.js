0747c94ea1008e660a333f5ead00673d6b6fad38
Dinesh <dineshpattnaik23@gmail.com>
2021-09-29 20:28:25 +0530

    Database connected and insert functionality under development

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 //jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-let newItems = [];
-let workItems = [];
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todoList"
+});
+
+const item2 = new Item({
+    name: "Press + to add new item"
+});
+
+const item3 = new Item({
+    name: "Check the checkbox to delete new item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems,function (err) {
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Successfully inserted");
+    }
+})
 
 app.get('/', function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options)
+    
     res.render('list', {
-        listTitle: day,
-        newListItems: newItems
+        listTitle: "Today",
+        newListItems: defaultItems
     });
 });
 
@@ -39,10 +62,10 @@ app.post('/work', function (req, res) {
 
 app.post('/', function (req, res) {
     let newItem = req.body.item;
-    if(req.body.list==="Work"){
+    if (req.body.list === "Work") {
         workItems.push(newItem);
         res.redirect('/work');
-    }else{
+    } else {
         newItems.push(newItem);
     }
     res.redirect('/');
